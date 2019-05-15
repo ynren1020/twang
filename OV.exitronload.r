c861c9c028bb912e26eece4f1ff721fca4e588dc
ynren1020 <renxx275@umn.edu>
2019-05-15 14:24:18 -0500

    Add files via upload

--- OV.exitronload.r
+++ OV.exitronload.r
@@ -2,6 +2,7 @@
 ##how many rows for each unique id is called exitron load########
 ##separate patient into two groups based on exitron load#########
 ##top 25% and bottom 25%#########################################
+##OV,STAD...
 #################################################################
 
 ##load package##
@@ -11,7 +12,12 @@ library("edgeR")
 library("limma")
 
 ##read data##
-input<-"OV.exitron"
+#input<-"OV.exitron"
+#input<-"STAD.exitron"
+#input<-"ESCA.exitron"
+#input<-"LAML.exitron"
+input<-"PRAD.exitron"
+#input<-"UCEC.exitron"
 load<-read.delim(input,header=FALSE,stringsAsFactors = FALSE)
 
 ##calculate exitron load##
@@ -20,29 +26,29 @@ loadstat<-load%>%
   summarize(load=n())
 
 ##select top and bottom 25%##
-load.q75<-quantile(loadstat$load,probs = 0.75,na.rm = TRUE) #388
-load.q25<-quantile(loadstat$load,probs = 0.25,na.rm = TRUE) #192
+load.q75<-quantile(loadstat$load,probs = 0.75,na.rm = TRUE) #162
+load.q25<-quantile(loadstat$load,probs = 0.25,na.rm = TRUE) #106
 
 loadstat.sub<-loadstat[loadstat$load>=load.q75|loadstat$load<=load.q25,]
 
 ##top 25->high, bottom 25->low##
 for (i in 1:nrow(loadstat.sub)){
-  loadstat.sub$group[i]<-ifelse(loadstat.sub$load[i]>=388,"high","low")
+  loadstat.sub$group[i]<-ifelse(loadstat.sub$load[i]>=load.q75,"high","low")
 }
 
-highload<-loadstat.sub$V16[loadstat.sub$group=="high"]
-lowload<-loadstat.sub$V16[loadstat.sub$group=="low"]
+highload<-loadstat.sub$V16[loadstat.sub$group=="high"]#141
+lowload<-loadstat.sub$V16[loadstat.sub$group=="low"]#147
 
 ##read gene count file## 
-input2<-"OV.txt"
+input2<-"UCEC.txt"
 count<-read.delim(input2,header = TRUE,row.names="GeneID",stringsAsFactors = FALSE,check.names = FALSE)
 countsub<-count[,colnames(count)%in%loadstat.sub$V16|colnames(count)=="Length"]
 ##DEG analysis##
 data <- countsub[,seq(2,ncol(countsub))]
 ##low first (65) high second (65)
 setcolorder(data, c(lowload,highload))
-group <- factor(c(rep(1,65),rep(2,65)))
-
+#group <- factor(c(rep(1,65),rep(2,65)))
+group <- factor(c(rep(1,length(lowload)),rep(2,length(highload))))
 ##create DEG object##
 y <- DGEList(counts=data, group=group)
 y$genes <- data.frame(Length=countsub$Length)
@@ -50,7 +56,7 @@ rownames(y$genes) <- rownames(y$counts)
   
 # filtering out low expressed genes
 #  the minimum number of samples in each group is 65, over here.
-keep <- rowSums(cpm(y)>1) >= 65
+keep <- rowSums(cpm(y)>1) >= min(highload,lowload)
 table(keep)
 y <- y[keep, keep.lib.sizes=FALSE]
 
@@ -62,14 +68,14 @@ design
 y <- calcNormFactors(y)
 y$samples
 
-write.table(rpkm(y), file=paste0('OV.rpkm','.exitronload.txt'), sep='\t', row.names = TRUE, quote = FALSE)
+#write.table(rpkm(y), file=paste0('UCEC.rpkm','.exitronload.txt'), sep='\t', row.names = TRUE, quote = FALSE)
 
 # normalise the read counts with 'voom' function
 v <- voom(y,design,plot = TRUE)
 # extract the normalised read counts
 counts.voom <- v$E
 # save normalised expression data into output dir
-write.table(counts.voom,file="counts.voom.txt",row.names=T,quote=F,sep="\t")
+#write.table(counts.voom,file="UCEC.counts.voom.txt",row.names=T,quote=F,sep="\t")
 
 # fit linear model for each gene given a series of libraries
 fit <- lmFit(v, design)
@@ -92,7 +98,7 @@ summary(decideTests(fit.2vs1, p.value=0.05,lfc=1))
 
 num = length(fit.2vs1$genes$Length)
 degs.2vs1 <- topTable(fit.2vs1, coef="group2 - group1", confint=TRUE, number = num)
-write.table(degs.2vs1, file=paste0('OV.highvslow','exitronload.degs.txt'), sep='\t',row.names = TRUE, quote = FALSE)
+#write.table(degs.2vs1, file=paste0('UCEC.highvslow','exitronload.degs.txt'), sep='\t',row.names = TRUE, quote = FALSE)
 
 ##read in splicing factor list##
 input3<-"splicing_factor_genes.txt"
@@ -100,45 +106,76 @@ splicing<-read.delim(input3,header = TRUE,stringsAsFactors = FALSE,check.names =
 
 degs.2vs1$gene<-rownames(degs.2vs1)
 
-degs.2vs1.splice<-full_join(degs.2vs1,splicing,by=c("gene"="Gene Symbol"))
-degs.2vs1.splice.sub<-na.omit(degs.2vs1.splice)
+#degs.2vs1.splice<-full_join(degs.2vs1,splicing,by=c("gene"="Gene Symbol"))
+#degs.2vs1.splice.sub<-na.omit(degs.2vs1.splice)
 
-degs.2vs1.splice.sub.pos<-degs.2vs1.splice.sub[degs.2vs1.splice.sub$logFC>0&degs.2vs1.splice.sub$adj.P.Val<=0.05,] #64
-degs.2vs1.splice.sub.neg<-degs.2vs1.splice.sub[degs.2vs1.splice.sub$logFC<0&degs.2vs1.splice.sub$adj.P.Val<=0.05,] #120
+#degs.2vs1.splice.sub.pos<-degs.2vs1.splice.sub[degs.2vs1.splice.sub$logFC>0&degs.2vs1.splice.sub$adj.P.Val<=0.05,] #191
+#degs.2vs1.splice.sub.neg<-degs.2vs1.splice.sub[degs.2vs1.splice.sub$logFC<0&degs.2vs1.splice.sub$adj.P.Val<=0.05,] #50
 
 
 ##From that we can select two list of genes to test for overlap significance using my homemade approach (fisher exact test, fisher.test):
 
-geneset1 <- degs.2vs1$gene[degs.2vs1$logFC>0&degs.2vs1$adj.P.Val<=0.05] #3224
+##negative (down)regulated##
+neg.geneset1 <- degs.2vs1$gene[degs.2vs1$logFC<0&degs.2vs1$adj.P.Val<=0.05] #3224
 geneset2 <- splicing$`Gene Symbol` #404
 
 universe <- length(
   unique(c(degs.2vs1$gene,geneset2))      #13901
 )
 
-common <- length(
+neg.common <- length(
   intersect(
-    unique(geneset1),               #64
+    unique(neg.geneset1),               #64
     unique(geneset2)
   )
 )
 
 
-mat <- matrix(
+mat.neg <- matrix(
   c(
-    universe - length(union(geneset1, geneset2)),
-    length(setdiff(geneset1, geneset2)),
-    length(setdiff(geneset2, geneset1)),
-    length(intersect(geneset1, geneset2))
+    universe - length(union(neg.geneset1, geneset2)),
+    length(setdiff(neg.geneset1, geneset2)),
+    length(setdiff(geneset2, neg.geneset1)),
+    length(intersect(neg.geneset1, geneset2))
   ),
   nrow=2
 )
 
-fr <- fisher.test(mat, alternative="greater")
-fr
+fr.neg <- fisher.test(mat.neg, alternative="greater")
+fr.neg$p.value
  
-                
+##postive (up) regulated##
+pos.geneset1 <- degs.2vs1$gene[degs.2vs1$logFC>0&degs.2vs1$adj.P.Val<=0.05] #3224
+#geneset2 <- splicing$`Gene Symbol` #404
 
+universe <- length(
+  unique(c(degs.2vs1$gene,geneset2))      #13901
+)
+
+pos.common <- length(
+  intersect(
+    unique(pos.geneset1),               #64
+    unique(geneset2)
+  )
+)
+
+
+mat.pos <- matrix(
+  c(
+    universe - length(union(pos.geneset1, geneset2)),
+    length(setdiff(pos.geneset1, geneset2)),
+    length(setdiff(geneset2, pos.geneset1)),
+    length(intersect(pos.geneset1, geneset2))
+  ),
+  nrow=2
+)
+
+fr.pos <- fisher.test(mat.pos, alternative="greater")
+fr.pos$p.value
+
+UCEC.plot<-data.frame(cohort="UCEC",neg.overlap=neg.common,neg.pval=fr.neg$p.value,pos.overlap=pos.common,pos.pval=fr.pos$p.value)                
+
+##OV##
 ##geneset1 <- degs.2vs1$gene[degs.2vs1$logFC<0&degs.2vs1$adj.P.Val<=0.05] 
 ##	Fisher's Exact Test for Count Data
 #data:  mat
@@ -149,3 +186,80 @@ fr
 #sample estimates:
 #  odds ratio 
 #1.81 
+
+##STAD##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value = 9e-07
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+#1.46  Inf
+#sample estimates:
+#odds ratio 
+#1.78 
+
+##ESCA##
+##negative##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value = 0.002
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+#1.23  Inf
+#sample estimates:
+#odds ratio 
+#1.64 
+
+##positive##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value = 0.003
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+#1.23  Inf
+#sample estimates:
+#odds ratio 
+#1.67 
+
+##LAML##
+##negative##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value = 2e-04
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+#2 Inf
+#sample estimates:
+#odds ratio 
+#3.5 
+
+##PRAD##
+##negative##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value = 0.006
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+# 1.1 Inf
+#sample estimates:
+#odds ratio 
+#      1.31 
+
+##UCEC##
+##positive##
+#Fisher's Exact Test for Count Data
+
+#data:  mat
+#p-value <2e-16
+#alternative hypothesis: true odds ratio is greater than 1
+#95 percent confidence interval:
+#2.46  Inf
+#sample estimates:
+#odds ratio 
+#2.93 
+
