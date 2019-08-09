57de6d40d6f0668fb8a2fbe41a59325232e7aaf3
ynren1020 <renxx275@umn.edu>
2019-08-09 10:59:06 -0500

    Add files via upload
    move first part of files to each cohort each group (Tumor /Normal) folder. Test if this script works.

--- file2folder.r
+++ file2folder.r
@@ -4,13 +4,13 @@
 library("filesstrings")
 library(tidyverse)
 
-dir.create("My_directory")
-file.create("My_file.txt")
-file.move("My_file.txt", "My_directory")
+#dir.create("My_directory")
+#file.create("My_file.txt")
+#file.move("My_file.txt", "My_directory")
 
 input<-"TCGA.metainfo.txt"
 meta<-read.delim(input,header = TRUE,stringsAsFactors = FALSE,sep = "\t")
-meta$fileid<-paste0(meta$FILE_ID,".txt")
+meta$fileid<-paste0(meta$FILE_ID,".bam.txt")
 ##Solid Tissue Normal--normal,others--tumor##
 for (i in 1:nrow(meta)){
   meta$group[i]<-ifelse(meta$TUMOR_TYPE[i]=="Solid Tissue Normal","Normal","Tumor")
@@ -21,16 +21,22 @@ for (i in 1:nrow(meta)){
   meta$cohort[i]<-strsplit(meta$PROJECT[i],"-")[[1]][2]
 }
 
-input2<-"fc.file.test.10.txt"
+input2<-"tmp1_bam.txt"
 fcfile<-read.delim(input2,header = FALSE,stringsAsFactors = FALSE,sep = "\t")
 
 ##join fcfile and meta
 fcfile.join<-left_join(fcfile,meta,by=c("V1"="fileid"))
-fcfile.join$newfileid<-paste0(fcfile.join$V1,".",fcfile.join$group,".",fcfile.join$cohort)
-
+fcfile.join$newfileid<-paste0(fcfile.join$FILE_ID,".",fcfile.join$group,".",fcfile.join$cohort,".txt")
+fcfile.join$newpath<-paste0("/data/ryang/yren/pacbio/tcga/",fcfile.join$cohort,"/",fcfile.join$group)
+fcfile.join$oldpath<-paste0("/data/ryang/yren/pacbio/tcga/tmp_1/",fcfile.join$V1)
 ##rename file##
 file.rename(from, to)
 
 fcfile.join.cmd<-data.frame(cmd="filesstrings::file.rename(",from=fcfile.join$V1,to=fcfile.join$newfileid,end=")")
 
+##file move##
+#dir.create("My_directory")
+file.move("My_file.txt", "My_directory")
+fcfile.join.movecmd<-data.frame(cmd="filesstrings::file.move(",from=paste0('"',fcfile.join$oldpath,'",'),to=paste0('"',fcfile.join$newpath,'"'),end=")")
 
+write.table(fcfile.join.movecmd,"fcfile.join.movecmd.tmp1.txt",col.names = FALSE,row.names = FALSE,quote = FALSE,sep = " ")
